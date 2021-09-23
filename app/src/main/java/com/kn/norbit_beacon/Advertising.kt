f854aadf746dd1f2f39aa66f2c86e790f854f0a3
Håkon Anders <haakon8855@users.noreply.github.com>
2021-09-23 12:20:07 +0200

    Initial bluetooth things

--- app/src/main/java/com/kn/norbit_beacon/Advertising.kt
+++ app/src/main/java/com/kn/norbit_beacon/Advertising.kt
@@ -1,10 +1,17 @@
 package com.kn.norbit_beacon
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertisingSetParameters
+import android.bluetooth.le.BluetoothLeAdvertiser
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.fragment.findNavController
 import com.kn.norbit_beacon.databinding.FragmentSecondBinding
 
@@ -14,6 +21,7 @@ import com.kn.norbit_beacon.databinding.FragmentSecondBinding
 class Advertising : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private lateinit var advertiser: BluetoothLeAdvertiser
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,6 +31,20 @@ class Advertising : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
+        advertiser = BluetoothAdapter.getDefaultAdapter().bluetoothLeAdvertiser
+
+        val parameters: AdvertisingSetParameters = (AdvertisingSetParameters.Builder())
+            .setLegacyMode(true)
+            .setConnectable(false)
+            .setInterval(AdvertisingSetParameters.INTERVAL_HIGH)
+            .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_HIGH)
+            .build()
+
+        val data: AdvertiseData = AdvertiseData.Builder().setIncludeDeviceName(true).build()
+
+
+
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
