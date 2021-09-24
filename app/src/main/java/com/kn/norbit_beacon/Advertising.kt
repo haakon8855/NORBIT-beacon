050fa70cc7d3e843a54a3dcb333bd4e666350ce4
Emma Valen Rian <emmavr@stud.ntnu.no>
2021-09-24 11:12:29 +0200

    Sending bluetooth advertisement packets without data

--- app/src/main/java/com/kn/norbit_beacon/Advertising.kt
+++ app/src/main/java/com/kn/norbit_beacon/Advertising.kt
@@ -1,5 +1,8 @@
 package com.kn.norbit_beacon
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.AdvertiseData
@@ -14,6 +17,15 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.fragment.findNavController
 import com.kn.norbit_beacon.databinding.FragmentSecondBinding
+import android.bluetooth.le.AdvertisingSet
+
+import android.bluetooth.le.AdvertisingSetCallback
+import android.content.pm.PackageManager
+import android.util.Log
+import android.os.ParcelUuid
+import androidx.core.app.ActivityCompat
+import java.util.*
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -21,7 +33,10 @@ import com.kn.norbit_beacon.databinding.FragmentSecondBinding
 class Advertising : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
-    private lateinit var advertiser: BluetoothLeAdvertiser
+    private lateinit var advertiser : BluetoothLeAdvertiser
+    private val LOG_TAG : String = "noe"
+    private lateinit var currentAdvertisingSet : AdvertisingSet
+    private lateinit var callback : AdvertisingSetCallback
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,7 +47,8 @@ class Advertising : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-        advertiser = BluetoothAdapter.getDefaultAdapter().bluetoothLeAdvertiser
+        val bluetoothManager =
+            requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
 
         val parameters: AdvertisingSetParameters = (AdvertisingSetParameters.Builder())
             .setLegacyMode(true)
@@ -43,7 +59,47 @@ class Advertising : Fragment() {
 
         val data: AdvertiseData = AdvertiseData.Builder().setIncludeDeviceName(true).build()
 
+        advertiser = bluetoothManager.adapter.bluetoothLeAdvertiser;
 
+        callback = object : AdvertisingSetCallback() {
+            override fun onAdvertisingSetStarted(
+                advertisingSet: AdvertisingSet,
+                txPower: Int,
+                status: Int
+            ) {
+                Log.i(
+                    LOG_TAG, "onAdvertisingSetStarted(): txPower:" + txPower + " , status: "
+                            + status
+                )
+                currentAdvertisingSet = advertisingSet
+
+                // After onAdvertisingSetStarted callback is called, you can modify the
+                // advertising data and scan response data:
+                currentAdvertisingSet.setAdvertisingData(
+                    AdvertiseData.Builder().setIncludeDeviceName(true).setIncludeTxPowerLevel(true).build()
+                )
+
+                // Wait for onAdvertisingDataSet callback...
+                currentAdvertisingSet.setScanResponseData(
+                    AdvertiseData.Builder().addServiceUuid(ParcelUuid(UUID.randomUUID())).build()
+                )
+                // Wait for onScanResponseDataSet callback...
+            }
+
+            override fun onAdvertisingDataSet(advertisingSet: AdvertisingSet, status: Int) {
+                Log.i(LOG_TAG, "onAdvertisingDataSet() :status:$status")
+            }
+
+            override fun onScanResponseDataSet(advertisingSet: AdvertisingSet, status: Int) {
+                Log.i(LOG_TAG, "onScanResponseDataSet(): status:$status")
+            }
+
+            override fun onAdvertisingSetStopped(advertisingSet: AdvertisingSet) {
+                Log.i(LOG_TAG, "onAdvertisingSetStopped():")
+            }
+        }
+
+        advertiser.startAdvertisingSet(parameters, data, null, null, null, callback);
 
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
@@ -59,7 +115,12 @@ class Advertising : Fragment() {
         }
     }
 
+
     override fun onDestroyView() {
+
+        // When done with the advertising:
+        advertiser.stopAdvertisingSet(callback)
+
         super.onDestroyView()
         _binding = null
     }
