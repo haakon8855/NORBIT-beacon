8eb216a1960f540b66640bbc81c9d1ab6eae0386
Håkon Anders <haakon8855@users.noreply.github.com>
2021-09-28 15:29:27 +0200

    Moved FusedLocationFetcher to Advertising fragment

--- app/src/main/java/com/kn/norbit_beacon/Advertising.kt
+++ app/src/main/java/com/kn/norbit_beacon/Advertising.kt
@@ -15,21 +15,25 @@ import com.kn.norbit_beacon.databinding.FragmentSecondBinding
 import android.bluetooth.le.AdvertisingSet
 
 import android.bluetooth.le.AdvertisingSetCallback
+import android.location.Location
 import android.util.Log
 import android.os.ParcelUuid
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class Advertising : Fragment() {
+class Advertising : Fragment(), LocationProvider.Listener {
 
     private var _binding: FragmentSecondBinding? = null
     private lateinit var advertiser : BluetoothLeAdvertiser
     private val LOG_TAG : String = "noe"
     private lateinit var currentAdvertisingSet : AdvertisingSet
     private lateinit var callback : AdvertisingSetCallback
+    private lateinit var locationFetcher: FusedLocationFetcher
+    private lateinit var lastLocation: Location
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -41,6 +45,10 @@ class Advertising : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        Log.i("Created", "created")
+        // Set up location manager
+        locationFetcher = FusedLocationFetcher(requireActivity(), this)
+        initializeLocationFetcher()
 
         val bluetoothManager =
             requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
@@ -105,6 +113,49 @@ class Advertising : Fragment() {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
 
+
+        /*
+        //The mail button.
+        binding.fab.setOnClickListener { view ->
+            // Should now have last known location in 'this.lastLocation'
+            var snackbarText = ""
+            if (!locationFetcher.getRunning()) {
+                initializeLocationFetcher()
+            }
+            if (this::lastLocation.isInitialized) {
+                if (lastLocation != null) {
+                    val lat = lastLocation.latitude
+                    val lng = lastLocation.longitude
+                    val acc = lastLocation.accuracy
+                    val age = (System.currentTimeMillis()-lastLocation.time)/1000
+                    snackbarText = "Lat: $lat, Lng: $lng, Acc: $acc\nAge(sec): $age"
+                } else {
+                    snackbarText = "Location is null, missing permissions?"
+                }
+            } else{
+                snackbarText = "Location is uninitialized, missing permissions?"
+            }
+            Snackbar.make(view, snackbarText, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }*/
+    }
+
+    private fun initializeLocationFetcher() {
+        locationFetcher.setListener(this)
+        locationFetcher.startUpdates()
+    }
+
+    override fun onNewLocationUpdate(location: Location) {
+        onLocationUpdate(location)
+    }
+
+    override fun onLocationUpdate(location: Location?) {
+        Log.i("Updated", "updated")
+        if (location != null) {
+            // TODO: Dont update if accuracy is worse than earlier and/or greater than some threshold
+            lastLocation = location
+            Log.i("Location:", "d"+location.accuracy)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -115,8 +166,12 @@ class Advertising : Fragment() {
         }
     }
 
+    public fun moveToHome() {
+        findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+    }
 
     override fun onDestroyView() {
+        locationFetcher.stopUpdates()
 
         // When done with the advertising:
         advertiser.stopAdvertisingSet(callback)
